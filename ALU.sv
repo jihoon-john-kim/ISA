0730fa9a1a2d1aa10a38296843836d016841bda7
aribrin <abrin@ucsd.edu>
2023-12-01 17:26:32 -0800

    pc and alu

---
+++ ALU.sv
@@ -0,0 +1,48 @@
+// Engineer: 
+// 
+// Create Date:    2016.10.15
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//   combinational (unclocked) ALU
+
+import definitions::*;			  // includes package "definitions"
+module ALU(          
+  input [ 3:0] OP,				     // ALU opcode, part of microcode
+  input [ 7:0] INPUT_A,			  // data inputs
+               INPUT_B,
+
+  output logic [7:0] OUT,		  // or:  output reg [7:0] OUT,
+ 
+
+    );
+	
+	
+  always_comb begin
+
+	case(OP)
+		'b011 : OUT = INPUT_A xor INPUT_B; //xor rd rs
+	 	'b100 : OUT = INPUT_A + INPUT_B; // inc rd [imm]
+		'b101 : OUT = INPUT_A <<<< INPUT_B; //shl
+		'b110 : OUT = INPUT_A >>>> INPUT_B; //shr
+		'b111 : OUT = INPUT_A and INPUT_B;
+	endcase
+	
+	
+	"""case(OUT)
+	  16'b0 :   ZERO = 1'b1;
+	  default : ZERO = 1'b0;
+	endcase"""
+	//$display("ALU Out %d \n",OUT);
+  end
+
+endmodule
---
+++ PC.sv
@@ -0,0 +1,32 @@
+// program counter example
+module pc (
+  input	clk,
+  input reset,
+
+  input [9:0] pc_inc;
+   
+  """input ZERO,					//STATUS flags
+  input LESS,
+  
+  input REG_JMP,				//JMP activated flags
+  input ZERO_JMP,						
+  input LESS_JMP,
+  input MAGIC_JMP,
+  input E_JMP_ACTIVE,
+  input E_JMP_READY,
+  input [5:0] JMP_DIST,
+  input [1:0] MAGIC,"""
+  
+  output logic[9:0] p_ct);
+
+  always_ff @(posedge clk) 
+		if(reset)
+			p_ct <= 0;
+	"""	else if (MAGIC_JMP)
+			p_ct <= p_ct + {MAGIC, 1'b1};
+		else if (E_JMP_ACTIVE & E_JMP_READY)
+			p_ct <= p_ct + 2'b10;"""
+		else
+			p_ct <= p_ct + pc_inc;//((REG_JMP | (ZERO & ZERO_JMP) | (LESS & LESS_JMP)) ? {JMP_DIST[5], JMP_DIST[5], JMP_DIST[5], JMP_DIST[5], JMP_DIST} : 1'b1); 
+			
+endmodule
